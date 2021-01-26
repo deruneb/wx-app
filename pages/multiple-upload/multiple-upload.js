980aecc16a44b438f179e8b2d6bde96feb96cd96
一给窝里高高 <953727948@qq.com>
2021-01-26 17:09:54 +0800

    审核失败调整
    审核失败调整

--- pages/multiple-upload/multiple-upload.js
+++ pages/multiple-upload/multiple-upload.js
@@ -25,6 +25,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    this.setData({
+      openid: wx.getStorageSync('app_openid')
+    })
     if(wx.getStorageSync('multiple')){
       var ceshi = wx.getStorageSync('multiple');
       this.setData({
